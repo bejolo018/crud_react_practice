c110b2dd1a7ead32e4acd6123c69daa989ed3af3
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-21 22:02:24 -0500

    completed NameList, UI complete! Functionality next

--- crud/src/Components/NameList.js
+++ crud/src/Components/NameList.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ListGroup, ListGroupItem, Button } from 'reactstrap'
 
 const NameList = () => {
     return (
-        <div>
-            <h1>
-            NameList
-            </h1>
-        </div>
+        <ListGroup className="mt-4">
+            <ListGroupItem className="d-flex"> 
+                <strong>User one</strong>
+                <div className="ml-auto">
+                    <Link className="btn btn-warning mr-1" to="/edit/1">Edit</Link>
+                    <Button color ="danger" type="submit"> Delete</Button>
+                </div>
+
+            </ListGroupItem>
+
+        </ListGroup>
     )
 }
 

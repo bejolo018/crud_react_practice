99b392e47a915ddc6e1360ba3d95e5d4251923c0
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-09-21 21:47:30 -0500

    Created components, created router structure in app.js, finished UI for AddUser

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './Components/Home'
+import AddUser from './Components/AddUser'
+import EditUser from './Components/EditUser'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
-    <div className="App">
-      
+    <div style={{maxWidth: "30rem", margin: "4rem auto"}}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/add" component={AddUser}  />
+          <Route path="/edit/:id" component={EditUser}  />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
